c3ad92bc85bf8e0a4f204b1fbfdd5a219a4a13f3
vigneshkrishnamoorthy <42338477+vigneshkrishnamoorthy@users.noreply.github.com>
2019-06-20 17:01:48 +0530

    Added Chrome driver,Selenuim Server,scripts.

---
+++ GitProject/src/test/java/GitProject/GitProject/testcase001.java
@@ -0,0 +1,24 @@
+package GitProject.GitProject;
+
+public class testcase001 {
+
+	public testcase001() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		method1();
+
+	}
+
+	public static void method1()
+
+	{
+
+		System.out.println("Wrote the first test case");
+
+	}
+
+}
---
+++ GitProject/src/test/java/GitProject/GitProject/testcase002.java
@@ -0,0 +1,22 @@
+package GitProject.GitProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testcase002 {
+
+	public testcase002() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", ".\\BrowserDrivers\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com");
+	}
+
+}
